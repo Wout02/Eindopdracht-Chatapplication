0dea552dc63c1344ca9d382428be4c15839b914c
Wout <w.geuze@student.avans.nl>
2021-01-18 19:21:37 +0100

    added some unittests

---
+++ Tests/Tests.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Server;
+using Client;
+using Gui;
+using System.Net.Sockets;
+
+namespace Tests
+{
+
+    [TestClass]
+    public class GuiTests
+    {
+
+        [TestMethod]
+        public void TestgetUserInfoReg() 
+        {
+            DataBaseManager dataBaseManager = new DataBaseManager();
+            string userName = "Kees";
+            string passWord = "1234";
+            bool isTrue= dataBaseManager.getUserInfoReg(userName, passWord);
+            Assert.IsTrue(isTrue);
+        }
+
+        [TestMethod]
+        public void TestgetUserInfoLogIn()
+        {
+            DataBaseManager dataBaseManager = new DataBaseManager();
+            string userName = "Kees";
+            string passWord = "1234";
+            bool isFalse = dataBaseManager.getUserInfoLogIn(userName, passWord);
+            Assert.IsFalse(isFalse);
+        }
+    }
+
+    [TestClass]
+    public class ServerTests
+    {
+
+        [TestMethod]
+        public void TestWriteData()
+        {
+        }
+    }
+}
